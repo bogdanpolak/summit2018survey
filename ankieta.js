3e03dbaf6c3062d4d3910db2aa206803b8f13d2b
BSC Polska <bogdan.polak@bsc.com.pl>
2018-06-12 18:12:01 +0200

    Added login section. Moved data to separate file. WhenReady script fre the future.

--- ankieta.js
+++ ankieta.js
@@ -1,33 +1,3 @@
-summitSlots = [{
-	id:'wt1030', date:'2018-06-19T10:00+02:00', duration:75,
-	sessions: [
-		{track:0, speaker:'Marco Cantu', subject:'Delphi: Whats New and Whats Coming'}]
-}, {
-	id:'wt1215', date:'2018-06-19T12:15+02:00', duration:75,
-	sessions: [
-		{track:1, speaker:'Stefan Glienke', subject:'Efficient data structures in Delphi'},
-		{track:2, speaker:'Mikołaj Milewski', subject:'Jak APIć w Delphi?'}]
-}, {
-	id:'wt1445', date:'2018-06-19T14:45+02:00', duration:75,
-	sessions: [
-		{track:1, speaker:'Brian Long', subject:'IDE Productivity Tips'},
-		{track:2, speaker:'Zdzisław Sroczyński', subject:'Automatyzacja aplikacji w Delphi - kuzyni i dalsza rodzina'}]
-}, {
-	id:'sr1000', date:'2018-06-20T10:00+02:00', duration:75,
-	sessions: [
-		{track:1, speaker:'Marco Cantu', subject:'VCL Applications on Windows 10'},
-		{track:2, speaker:'Bogdan Polak', subject:'Migracja "dojrzałych" projektów VCL'}]
-}, {
-	id:'sr1130', date:'2018-06-20T11:30+02:00', duration:75,
-	sessions: [
-		{track:1, speaker:'Brian Long', subject:'How to Access the Android API'},
-		{track:2, speaker:'Piotr Chlebek', subject:'Propagacja wsteczna błędu w sieciach neuronowych'}]
-}, {
-	id:'sr1300', date:'2018-06-20T13:00+02:00', duration:75,
-	sessions: [
-		{track:1, speaker:'Stefan Glienke', subject:'Software testing - DUnit, Mocks and TestInsight'},
-		{track:2, speaker:'Mirosław Kardaś', subject:'Delphi dla elektronika'}]
-}];
 surveyResults = [
 	{session:'wt1030', track:0, rating:5},
 	{session:'sr1000', track:2, rating:3}, 
@@ -96,25 +66,32 @@ function generateSlot (surveyDiv, slot) {
 	}
 	surveyDiv.appendChild(slotDiv);
 }
-function generateSurvey (slots) {
-	const surveyDiv = document.getElementById('summit-survey');
-	slots.forEach( slot =>  generateSlot(surveyDiv, slot) );
-}
-
-function initSurvey () {
+function addOnClickEvents () {
 	$(".btn-group > .btn").click(function(){
 		const sessionName = this.parentElement.getAttribute("conferece-slot");
+		const trackNo = this.parentElement.getAttribute("conferece-track");
 		$('div[conferece-slot="'+sessionName+'"] > .btn').removeClass("active");
-		/*
-		this.parentElement.getElementsByClassName('btn').forEach(
-			elem => $(elem).removeClass("active"));
-		*/
-		// $(".btn-group > .btn").removeClass("active");
 		$(this).addClass("active");
-		console.log(sessionName+'='+this.innerHTML);
+		const sessionRating = this.innerHTML;
+		console.log({session:sessionName,track:trackNo,rating:sessionRating});
 	});
 }
+function generateSurvey (id,slots) {
+	const surveyDiv = document.getElementById(id);
+	slots.forEach( slot =>  generateSlot(surveyDiv, slot) );
+	addOnClickEvents();
+}
 
-generateSurvey(summitSlots);
-initSurvey ();
+function toggleDisplaySection (id) {
+	$('#'+id).toggle();
+}
+
+$( document ).ready( function() {
+	function ShowAll () {
+		toggleDisplaySection ('login');
+		toggleDisplaySection ('summit-survey');
+		generateSurvey('summit-survey',delphiDeveloperSummit2018Slots);
+	}
+	ShowAll ();
+});
 
