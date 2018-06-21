c5fde488e32a5e1d824bdd5e3a910ae9c43e0536
BSC Polska <bogdan.polak@bsc.com.pl>
2018-06-21 17:41:42 +0200

    Added laguage info in session data

--- www/dataSlots.js
+++ www/dataSlots.js
@@ -1,30 +1,30 @@
 const SlotsDelphiDeveloperSummit2018 = [{
 	id:'wt1030', date:'2018-06-19T10:00+02:00', duration:75,
 	sessions: [
-		{track:0, speaker:'Marco Cantu', subject:'Delphi: Whats New and Whats Coming'}]
+		{track:0, lang:'eng', speaker:'Marco Cantu', subject:'Delphi: Whats New and Whats Coming'}]
 }, {
 	id:'wt1215', date:'2018-06-19T12:15+02:00', duration:75,
 	sessions: [
-		{track:1, speaker:'Stefan Glienke', subject:'Efficient data structures in Delphi'},
-		{track:2, speaker:'Mikołaj Milewski', subject:'Jak APIć w Delphi?'}]
+		{track:1, lang:'eng', speaker:'Stefan Glienke', subject:'Efficient data structures in Delphi'},
+		{track:2, lang:'pl', speaker:'Mikołaj Milewski', subject:'Jak APIć w Delphi?'}]
 }, {
 	id:'wt1445', date:'2018-06-19T14:45+02:00', duration:75,
 	sessions: [
-		{track:1, speaker:'Brian Long', subject:'IDE Productivity Tips'},
-		{track:2, speaker:'Zdzisław Sroczyński', subject:'Automatyzacja aplikacji w Delphi - kuzyni i dalsza rodzina'}]
+		{track:1, lang:'eng', speaker:'Brian Long', subject:'IDE Productivity Tips'},
+		{track:2, lang:'pl', speaker:'Zdzisław Sroczyński', subject:'Automatyzacja aplikacji w Delphi - kuzyni i dalsza rodzina'}]
 }, {
 	id:'sr1000', date:'2018-06-20T10:00+02:00', duration:75,
 	sessions: [
-		{track:1, speaker:'Marco Cantu', subject:'VCL Applications on Windows 10'},
-		{track:2, speaker:'Bogdan Polak', subject:'Migracja "dojrzałych" projektów VCL'}]
+		{track:1, lang:'eng', speaker:'Marco Cantu', subject:'VCL Applications on Windows 10'},
+		{track:2, lang:'pl', speaker:'Bogdan Polak', subject:'Migracja "dojrzałych" projektów VCL'}]
 }, {
 	id:'sr1130', date:'2018-06-20T11:30+02:00', duration:75,
 	sessions: [
-		{track:1, speaker:'Brian Long', subject:'How to Access the Android API'},
-		{track:2, speaker:'Piotr Chlebek', subject:'Propagacja wsteczna błędu w sieciach neuronowych'}]
+		{track:1, lang:'eng', speaker:'Brian Long', subject:'How to Access the Android API'},
+		{track:2, lang:'pl', speaker:'Piotr Chlebek', subject:'Propagacja wsteczna błędu w sieciach neuronowych'}]
 }, {
 	id:'sr1300', date:'2018-06-20T13:00+02:00', duration:75,
 	sessions: [
-		{track:1, speaker:'Stefan Glienke', subject:'Software testing - DUnit, Mocks and TestInsight'},
-		{track:2, speaker:'Mirosław Kardaś', subject:'Delphi dla elektronika'}]
+		{track:1, lang:'eng', speaker:'Stefan Glienke', subject:'Software testing - DUnit, Mocks and TestInsight'},
+		{track:2, lang:'pl', speaker:'Mirosław Kardaś', subject:'Delphi dla elektronika'}]
 }];
